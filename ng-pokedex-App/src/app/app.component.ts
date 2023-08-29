734b6553075104a77297ff350208db1be9d187d6
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-08-29 20:42:12 +0200

    update

--- ng-pokedex-App/src/app/app.component.ts
+++ ng-pokedex-App/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: ` <h1>Bienvenue dans mon application Angular {{ title }}!</h1> `,
+  template: `<h1>Bienvenue dans mon Pokedex Angular {{ pokemons[0] }} !</h1>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = "ng-pokedex-App";
+  pokemons = ["Bulbizarre", "Salaméche", "Carapuce"];
+
+  ngOnInit(): void {
+    console.table(this.pokemons);
+  }
 }
