e78b721d94178eb6ae1fa17225b263c0e45b91dc
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-09-28 14:41:53 +0200

    Login Acess

--- cocktail/src/pages/auth/login.js
+++ cocktail/src/pages/auth/login.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./auth.css";
 
 const Login = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("formulaire");
+    console.log(login, password);
+  };
   return (
     <div className="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="group">
           <label htmlFor="login"> Identifiant</label>
-          <input type="text" name="login" />
+          <input
+            type="text"
+            name="login"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
         </div>
         <div className="group">
           <label htmlFor="password"> Mot de passe</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <button>Connexion</button>
