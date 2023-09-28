cc88772ab776511f9e12c401ab848d228ac2b86a
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-09-28 08:42:05 +0200

    Update App.js

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <Home />
+        <Service />
+        <Contact />
       </header>
     </div>
   );
