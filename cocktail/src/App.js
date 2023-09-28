ea8b962e237fcd85ea2ac8d9203e776a516a0695
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-09-28 08:58:37 +0200

    routing

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "@/pages/Home";
 import Service from "@/pages/Service";
@@ -7,12 +6,9 @@ import Contact from "@/pages/Contact";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Home />
-        <Service />
-        <Contact />
-      </header>
+      <Home />
+      <Service />
+      <Contact />
     </div>
   );
 }
