ca7439b5fc465ae19530aca348b4591eccc21490
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-09-28 17:15:21 +0200

    listUser

--- cocktail/src/pages/admin/User/User.js
+++ cocktail/src/pages/admin/User/User.js
@@ -1,24 +1,69 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { userService } from "../../../_services/user.service";
 
 const User = () => {
-  let navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const flag = useRef(false);
 
+  // Récupération de la liste des utilisateurs à l'affichage
   useEffect(() => {
-    userService
-      .getAllUser()
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+    if (flag.current === false) {
+      userService
+        .getAllUsers()
+        .then((res) => {
+          setUsers(res.data.data);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    return () => (flag.current = true);
   }, []);
-  const rayan = (userId) => {
-    console.log("click");
-    navigate("../edit/" + userId);
+
+  // Gestion du bouton de suppression d'un utilisateur
+  const delUser = (userId) => {
+    userService
+      .deleteUser(userId)
+      .then((res) => {
+        // Mise à jour du state pour affichage
+        setUsers((current) => current.filter((user) => user.id !== userId));
+      })
+      .catch((err) => console.log(err));
   };
+
   return (
     <div className="User">
-      User Liste
-      <button onClick={() => rayan(4)}>User 4</button>
+      User liste
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Email</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <span className="del_ubtn" onClick={() => delUser(user.id)}>
+                  X
+                </span>
+              </td>
+              <td>
+                <Link to={`/admin/user/edit/${user.id}`}>{user.id}</Link>
+              </td>
+              <td>{user.nom}</td>
+              <td>{user.prenom}</td>
+              <td>{user.email}</td>
+              <td>{user.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
