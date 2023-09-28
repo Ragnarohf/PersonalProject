7e1cc64c06307253a03c57bd766b5b093b2ae6b7
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-09-28 10:30:51 +0200

    PublicRoute

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
-import Home from "./pages/Home";
-import Service from "./pages/Service";
-import Contact from "./pages/Contact";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Error from "./_utils/Error";
+import { BrowserRouter } from "react-router-dom";
+import PublicRoute from "./pages/public/PublicRoute";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-
-          <Route path="/home" element={<Home />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <PublicRoute />
       </BrowserRouter>
     </div>
   );
